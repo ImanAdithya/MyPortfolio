6b7a509f3ecdb5a7f114b40374c66425f9b38c74
ImanAdithya <adithyaiman07@gmail.com>
2023-05-17 20:15:25 +0530

    updated

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    Particles.init({
+        selector: '.background',
+        maxParticles: 150,
+        connectParticles: 'true',
+        speed:2,
+        minDistance: 140,
+        sizeVariations: 4,
+        color: '#ffffff'
+    });
+}
