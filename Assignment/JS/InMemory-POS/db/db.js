7e4500967b5c5cb2909b0023e5b861d0a142794f
ImanAdithya <adithyaiman07@gmail.com>
2023-06-21 07:27:20 +0530

    item db update

--- Assignment/JS/InMemory-POS/db/db.js
+++ Assignment/JS/InMemory-POS/db/db.js
@@ -3,3 +3,9 @@ let customerDetail=[
     {customerID:"C00-002",customerName:"Saman",customerAddress:"Galle",customerContact:"0767849234"},
     {customerID:"C00-003",customerName:"Namal",customerAddress:"Jaffna",customerContact:"0745679213"}
 ];
+
+let itemDetails= [
+    {itemID:"I00-001",itemDescription:"Volley Ball",itemUnitPrice:7000,itemQty:1},
+    {itemID:"I00-002",itemDescription:"wickets",itemUnitPrice:100,itemQty:3},
+    {itemID:"I00-003",itemDescription:"Bat",itemUnitPrice:7000,itemQty:1}
+];
